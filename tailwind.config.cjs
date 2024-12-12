42c81b25c1e938f08654ba9588a71d8d7222321f
kojofocus <ernestobimpeh9@gmail.com>
2024-12-12 06:32:13 +0000

    fixed the module export error

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+// CommonJS syntax
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [plugin],
+};
